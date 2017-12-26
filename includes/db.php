af09f5a5b62c6d28df3cdc7dc2cd6a24e6a5c416
kratos619 <gaurav.palaspagar@hotmail.com>
2017-12-26 06:57:55 +0530

    refactoring code

---
+++ includes/db.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gaurav
+ * Date: 25-12-2017
+ * Time: 18:16
+ */
+
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pass'] = 1234;
+$db['db_name'] = 'blog_cms';
+
+foreach ($db as $item => $value){
+    define(strtoupper($item),$value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if($connection){
+    echo "we r connected";
+}
